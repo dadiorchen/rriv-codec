45bc84c592a1d52a9088c8e256011f52f70a4de9
Dadiorchen <antlet2004@gmail.com>
2025-07-27 23:32:56 +0000

    feat: copy naive encode

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,31 @@ pub fn decode(data: &[f64]) -> DecodedData {
     }
 }
 
+pub fn encode_naive( timestamp: i64, 
+               values: &[f32]
+             ) -> Box<[u8]>{
+    
+    println!("encode {} {}", timestamp, values[0]);
+    // rprintln!("{} values", values.len());
+    let mut bytes: [u8; 22] = [0; 22];
+    // let timestamp_bytes = timestamp.to_le_bytes();
+    // rprintln!("{:X?}", timestamp_bytes);
+    let timestamp_bytes = timestamp.to_be_bytes();
+    println!("{:X?}", timestamp_bytes);
+    bytes[0..8].copy_from_slice(&timestamp_bytes);
+    for i in 0..values.len(){
+      let value = (values[i] * 100.0) as u32;
+      let value_bytes = value.to_be_bytes();
+      println!("{:?}", (i * 4 + 8)..(i * 4 + 12));
+      bytes[(i * 4 + 8)..(i * 4 + 12)].copy_from_slice(&value_bytes);
+      println!("{:X?}", value_bytes);
+      if i == 2 { break }; // send up to 3 values
+    }
+    
+    println!("{:X?}", bytes);
+    return Box::new(bytes);
+}
+
 // write test
 #[cfg(test)]
 mod tests {
@@ -96,11 +121,54 @@ mod tests {
         assert_eq!([8.380151764e-315, 24.4, 24.5, 24.9, 25.9, 28.1, 30.2, 10.0], result.as_ref());
         assert_eq!(mem::size_of_val(&result), 16); 
 
+        let total = result.len() * mem::size_of::<f64>();
+        println!("size of f64: {}", mem::size_of::<f64>());
+        println!("length of result: {}", result.len());
+        println!("Total size: {}", total);
+        assert_eq!(total, 64);
+
+
         // decode the result, the result is a struct with timestamp, temperature and humidity
         let result_decoded = decode(&result);
         assert_eq!(result_decoded.timestamp, timestamp_i64);
         assert_eq!(result_decoded.temperature, temperatures);
         assert_eq!(result_decoded.humidity, humidity);
+
+
+    }
+
+    #[test]
+    fn test_encode_naive() {
+        let time_str = "2023-10-01T12:00:00+00:00";
+        let datetime = DateTime::parse_from_rfc3339(time_str).expect("Failed to parse date");
+        // use i64 to store the timestamp
+        let timestamp_i64 = datetime.timestamp();
+        // create date from timestamp
+        let datetime_parsed = DateTime::<Utc>::from_timestamp(timestamp_i64, 0)
+            .expect("Failed to create date from timestamp");
+        println!("Parsed datetime: {}", datetime);
+        println!("Unix timestamp: {}", datetime.timestamp());
+        println!("Parsed timestamp: {}", datetime_parsed.timestamp());
+        println!("Parsed timestamp in text: {}", datetime_parsed.to_rfc3339());
+        let temperatures = [24.4, 24.5, 24.9, 25.9, 28.1, 30.2];
+        let humidity = 10;
+        let result = encode_naive(timestamp_i64, temperatures.as_ref());
+        println!("Encoded data size: {}", result.len());
+        println!("Encoded data use space: {} bytes", mem::size_of_val(&result));
+        assert_eq!(result.len(), 22); 
+        println!("{:X?}", result);
+        assert_eq!([0, 0, 0, 0, 0x65, 0x19, 0x5F, 0x40, 0, 0, 0x9, 0x88, 0, 0, 0x9, 0x92, 0, 0, 9, 0xBA, 0, 0], result.as_ref());
+        //assert_eq!([8.380151764e-315, 24.4, 24.5, 24.9, 25.9, 28.1, 30.2, 10.0], result.as_ref());
+        assert_eq!(mem::size_of_val(&result), 16); 
+        let total = result.len() * mem::size_of::<u8>();
+        println!("Total size: {}", total);
+        assert_eq!(total, 22);
+
+        // decode the result, the result is a struct with timestamp, temperature and humidity
+//        let result_decoded = decode(&result);
+//        assert_eq!(result_decoded.timestamp, timestamp_i64);
+//        assert_eq!(result_decoded.temperature, temperatures);
+//        assert_eq!(result_decoded.humidity, humidity);
 
 
     }
