a8fc812d7558fe2db44c592092bf4cb108465cb6
Dadiorchen <antlet2004@gmail.com>
2025-07-30 06:08:03 +0000

    chore: semi, back to finish encode v1

--- src/utils.rs
+++ src/utils.rs
@@ -58,3 +58,23 @@ pub fn getBits32(num: u32, bits: usize) -> Bits {
 }
 
 
+// print hex readable
+pub fn hex_dump(data: &[u8]) {
+    for (i, chunk) in data.chunks(16).enumerate() {
+        print!("{:08x}  ", i * 16);
+        for byte in chunk {
+            print!("{:02x} ", byte);
+        }
+        // Print ASCII representation
+        print!(" {:>width$}", "", width = (16 - chunk.len()) * 3);
+        for byte in chunk {
+            let c = if (0x20..0x7F).contains(byte) {
+                *byte as char
+            } else {
+                '.'
+            };
+            print!("{}", c);
+        }
+        println!();
+    }
+}
